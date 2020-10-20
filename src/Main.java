b41749a67d2d45ea59c4936388aa49c7d02c256b
forynski <65871510+forynski@users.noreply.github.com>
2020-10-20 17:53:14 +0100

    stage 1/4 refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,114 +1,115 @@
 package banking;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    static List<Account> accountList = new ArrayList<>();
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("1. Create an account");
-            System.out.println("2. Log into account");
-            System.out.println("0. Exit");
-
-            int choice = Integer.parseInt(scanner.nextLine());
-            if (choice == 0) {
-                System.out.println("Exiting...");
-                System.exit(0);
-            }
-            switch (choice) {
-                case 1:
-                    createAccount();
-                    break;
-                case 2:
-                    if (logIn()) {
-                        authenticatedUserMenu();
-                    }
-            }
-        }
-    }
-
-    private static void authenticatedUserMenu() {
-        System.out.println("You have successfully logged in!");
-        System.out.println();
-        loop:
-        while (true) {
-            System.out.println("1. Balance");
-            System.out.println("2. Log out");
-            System.out.println("0. Exit");
-            int choice = Integer.parseInt(scanner.nextLine());
-            if (choice == 0) {
-                System.out.println("Bye!");
-                System.exit(0);
-            }
-            switch (choice) {
-                case 1:
-                    printBalance();
-                    break;
-                case 2:
-                    logOut();
-                    break loop;
-            }
-        }
-    }
-
-    private static void logOut() {
-        System.out.println();
-        System.out.println("You have successfully logged out!");
-        System.out.println();
-    }
-
-    private static void printBalance() {
-        Double balance = 0.0;
-        System.out.println();
-        System.out.println("Balance: " + balance);
-        System.out.println();
-    }
-
-    private static boolean logIn() {
-        System.out.println();
-        System.out.println("Enter your card number:");
-        String loginCardNumber = scanner.nextLine();
-        Optional<Account> filteredAccount = accountList.stream()
-                .filter(account -> account.getCardNumber().equals(loginCardNumber))
-                .findFirst();
-        if (filteredAccount.isEmpty()) {
-            System.out.println();
-            System.out.println("Wrong card number or PIN!");
-            System.out.println();
-            return false;
-        } else {
-            System.out.println("Enter your PIN!");
-            String loginPin = scanner.nextLine();
-            if (filteredAccount.get().getPin().equals(loginPin)) {
-                System.out.println();
-                return true;
-            } else {
-                System.out.println();
-                System.out.println("Wrong card number or PIN!");
-                System.out.println();
-            }
-        }
-        return false;
-    }
-
-    private static void createAccount() {
-        System.out.println();
-        int IIN = 400000;
-        int accountNum = ThreadLocalRandom.current().nextInt(100_000_000, 1_000_000_000);
-        int checksum = 1;
-        String cardNum = String.valueOf(IIN) + accountNum + checksum;
-        String pin = String.valueOf(ThreadLocalRandom.current().nextInt(1000, 10000));
-        Account createdAccount = new Account(cardNum, pin);
-        accountList.add(createdAccount);
-        System.out.println("Your card has been created");
-        System.out.println("Your card number: \n" + createdAccount.getCardNumber());
-        System.out.println("Your card PIN: \n" + createdAccount.getPin());
-        System.out.println();
+        Account account = new Account();
+        account.menu();
     }
 }
+
+class Account {
+    Map<String, Map<String, Integer>> account = new HashMap<>();
+    Scanner scanner = new Scanner(System.in);
+    String acc;
+    Integer pin;
+
+    void menu() {
+        System.out.println("1. Create an account");
+        System.out.println("2. Log into account");
+        System.out.println("0. Exit");
+
+        String key = scanner.nextLine();
+
+        switch (key) {
+            case "1":
+                System.out.println("Your card has been created");
+                System.out.println("Your card number:");
+                System.out.println(create());
+                System.out.println("Your card PIN:");
+                System.out.format("%04d", account.get(acc).get("PIN"));
+                System.out.println();
+                menu();
+                break;
+            case "2":
+                System.out.println("Enter your card number");
+                acc = scanner.nextLine();
+                System.out.println("Enter your PIN:");
+                pin = Integer.parseInt(scanner.nextLine());
+                if (account.containsKey(acc) && account.get(acc).get("PIN").equals(pin)) {
+                    System.out.println("You have successfully logged in!");
+                    balance();
+                } else {
+                    System.out.println("Wrong card number or PIN!");
+                    menu();
+                }
+                break;
+            case "0":
+                exit();
+                break;
+            default:
+                System.out.println("\nIncorrect option! Try again.\n");
+                break;
+        }
+    }
+
+    void balance() {
+        System.out.println("1. balance");
+        System.out.println("2. Log out");
+        System.out.println("0. Exit");
+        String key = scanner.nextLine();
+        switch (key) {
+            case "1":
+                balanceShow();
+                balance();
+                break;
+            case "2":
+                System.out.println("You have successfully logged out!");
+                menu();
+                break;
+            case "0":
+                exit();
+                break;
+            default:
+                System.out.println("\nIncorrect option! Try again.\n");
+                break;
+        }
+    }
+
+    public void balanceShow() {
+        System.out.println(account.get(acc).get("BALANCE"));
+    }
+
+    public String create() {
+
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        Map<String, Integer> info = new HashMap<>();
+        // first not 0 digit
+        sb.append(random.nextInt(9) + 1);
+        for (int i = 0; i < 9; i++) {
+            sb.append(random.nextInt(10));
+        }
+
+        acc = 400000 + sb.toString();
+        StringBuilder pin = new StringBuilder();
+
+        // pin
+        for (int i = 0; i < 4; i++) {
+            pin.append(random.nextInt(10));
+        }
+        info.put("PIN", Integer.parseInt(pin.toString()));
+        info.put("BALANCE", 0);
+        account.put(acc, info);
+        return acc;
+    }
+
+    void exit() {
+        System.out.println("\nBye!");
+    }
+}
+
