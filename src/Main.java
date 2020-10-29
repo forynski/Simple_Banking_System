e1f62630100fa959ad5e6e6364def6e1b3475ac8
forynski <65871510+forynski@users.noreply.github.com>
2020-10-29 17:04:43 +0000

    stage 4/4 final refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,221 +1,168 @@
 package banking;
 
-import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Main {
-
-    static String fileName;
-
     public static void main(String[] args) {
-
-        fileName = "card.s3db";
-
-        createNewTable();
+        String path = "jdbc:sqlite:" + args[1];
+        Database data = new Database(path);
+        data.getDatabase();
         Account account = new Account();
-        account.menu();
+        account.menu(data);
+    }
+}
 
+class Account {
+    Map<String, Map<String, String>> account = new HashMap<>();
+    Scanner scanner = new Scanner(System.in);
+    String acc;
+    String pin;
+    Integer income;
+    void menu(Database data) {
+        System.out.println("1. Create an account");
+        System.out.println("2. Log into account");
+        System.out.println("0. Exit");
+
+        String key = scanner.nextLine();
+
+        switch (key) {
+            case "1":
+                System.out.println("Your card has been created");
+                System.out.println("Your card number:");
+                acc = create();
+                System.out.println(acc);
+                System.out.println("Your card PIN:");
+                pin = account.get(acc).get("PIN");
+                System.out.format(pin);
+                data.addRowToDatabase(acc, pin);
+                System.out.println();
+                menu(data);
+                break;
+            case "2":
+                System.out.println("Enter your card number");
+                acc = scanner.nextLine();
+                System.out.println("Enter your PIN:");
+                pin = scanner.nextLine();
+                if (data.cardExistInDatabase(acc, pin)) {
+                    System.out.println("You have successfully logged in!");
+                    balance(data);
+                } else {
+                    System.out.println("Wrong card number or PIN!");
+                    menu(data);
+                }
+                break;
+            case "0":
+                exit();
+                break;
+            default:
+                System.out.println("\nIncorrect option! Try again.\n");
+                menu(data);
+        }
     }
 
-    private static void createNewTable() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:" + fileName;
-
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS card (\n"
-                + "	id INTEGER NOT NULL PRIMARY KEY,\n"
-                + "	number TEXT,\n"
-                + "	pin TEXT,\n"
-                + " balance INTEGER DEFAULT 0"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+    void balance(Database data) {
+        System.out.println("1. Balance");
+        System.out.println("2. Add income");
+        System.out.println("3. Do transfer");
+        System.out.println("4. Close account");
+        System.out.println("5. Log out");
+        System.out.println("0. Exit");
+        String key = scanner.nextLine();
+        switch (key) {
+            case "1":
+                System.out.println("Balance: " + data.getBalanse(acc));
+                balance(data);
+                break;
+            case "2":
+                System.out.println("Enter income:");
+                income = Integer.parseInt(scanner.nextLine());
+                data.addIncome(income,acc);
+                System.out.println("Income was added!");
+                balance(data);
+            case "5":
+                System.out.println("You have successfully logged out!");
+                menu(data);
+                break;
+            case "3":
+                System.out.println("Enter card number:");
+                String numberTo = scanner.nextLine();
+                System.out.println(data.cardExistsError(acc,numberTo));
+                if (data.cardExistsError(acc,numberTo).equals("Enter how much money you want to transfer:")){
+                    income = Integer.parseInt(scanner.nextLine());
+                    data.transfer(income,acc,numberTo);
+                };
+                balance(data);
+            case "4":
+                data.deleteAccount(acc);
+                menu(data);
+            case "0":
+                exit();
+                break;
+            default:
+                System.out.println("\nIncorrect option! Try again.\n");
+                break;
         }
     }
 
-    static class Account {
-        Map<String, Map<String, Integer>> account = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        String acc;
-        Integer pin;
-        Integer balance;
+    public String create() {
 
-        void menu() {
-            System.out.println("1. Create an account" + "\n" + "2. Log into account" + "\n" + "0. Exit");
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> info = new HashMap<>();
+        // first not 0 digit
+        sb.append(random.nextInt(9) + 1);
+        for (int i = 0; i < 8; i++) {
+            sb.append(random.nextInt(10));
+        }
 
+        acc = 400000 + sb.toString();
+        acc = acc + luhnAlgLastNumber(acc);
+        StringBuilder pin = new StringBuilder();
 
-            String key = scanner.nextLine();
+        // pin
+        for (int i = 0; i < 4; i++) {
+            pin.append(random.nextInt(10));
+        }
+        info.put("PIN", pin.toString());
+        info.put("BALANCE", "0");
+        account.put(acc, info);
+        return acc;
+    }
 
-            switch (key) {
-                case "1":
-                    System.out.println("Your card has been created");
-                    System.out.println("Your card number:");
-                    System.out.println(create());
-                    System.out.println("Your card PIN:");
-                    System.out.format("%04d", account.get(acc).get("PIN"));
-                    System.out.println();
-                    menu();
-                    break;
-                case "2":
-                    System.out.println("Enter your card number");
-                    acc = scanner.nextLine();
-                    System.out.println("Enter your PIN:");
-                    pin = Integer.parseInt(scanner.nextLine());
-                    if (account.containsKey(acc) && account.get(acc).get("PIN").equals(pin)) {
-                        System.out.println("You have successfully logged in!");
-                        balance();
-                    } else {
-                        System.out.println("Wrong card number or PIN!");
-                        menu();
-                    }
-                    break;
-                case "0":
-                    exit();
-                    break;
-                default:
-                    System.out.println("\nIncorrect option! Try again.\n");
-                    break;
+    public static int luhnAlgLastNumber(String acc) {
+        int sum = 0;
+        char ch;
+        String s;
+        int[] arr = new int[acc.length()];
+        for (int i = 0; i < acc.length(); i++) {
+            ch = acc.charAt(i);
+            if (Character.isDigit(ch)) {
+                arr[i] = Character.getNumericValue(ch);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                arr[i] = arr[i] * 2;
             }
         }
 
-        void balance() {
-            System.out.println("1. Balance");
-            System.out.println("2. Add income");
-            System.out.println("3. Do transfer");
-            System.out.println("4. Close account");
-            System.out.println("5. Log out");
-            System.out.println("0. Exit");
-            String key = scanner.nextLine();
-            switch (key) {
-                case "1":
-                    balanceShow();
-                    balance();
-                    break;
-                case "2":
-                    balanceAdd();
-                    balance();
-                    break;
-                case "3":
-                    // do transfer
-                case "4":
-                    // close account
-                case "5":
-                    System.out.println("You have successfully logged out!");
-                    menu();
-                    break;
-                case "0":
-                    exit();
-                    break;
-                default:
-                    System.out.println("\nIncorrect option! Try again.\n");
-                    break;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 9) {
+                arr[i] = arr[i] - 9;
             }
         }
 
-        public void balanceShow() {
-            System.out.println(account.get(acc).get("BALANCE"));
+        for (int i : arr) {
+            sum += i;
         }
+        return sum % 10 == 0 ? 0 : 10 - sum % 10;
+    }
 
-        public void balanceAdd() {
-            System.out.println("Enter income: ");
-            balance = Integer.parseInt(scanner.nextLine());
-
-            String url = "jdbc:sqlite:" + fileName;
-            String sql = "INSERT INTO card (balance) VALUES(?)";
-
-            try (Connection conn = DriverManager.getConnection(url);
-                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-                // Setting parameters
-
-                pstmt.setInt(1, balance);
-                pstmt.executeUpdate();
-
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-
-            }
-
-        }
-
-        public String create() {
-
-            Random random = new Random();
-            StringBuilder sb = new StringBuilder();
-            Map<String, Integer> info = new HashMap<>();
-            // first digit is not 0
-            sb.append(random.nextInt(9) + 1);
-            for (int i = 0; i < 8; i++) {
-                sb.append(random.nextInt(10));
-            }
-
-            acc = 400000 + sb.toString();
-            StringBuilder pin = new StringBuilder();
-
-            // Luhn algorithm
-            int checksum = getChecksum();
-            String lastDigit = String.valueOf(checksum);
-            acc = 400000 + sb.toString() + checksum;
-
-
-            // pin
-            for (int i = 0; i < 4; i++) {
-                pin.append(random.nextInt(10));
-            }
-
-            info.put("PIN", Integer.parseInt(pin.toString()));
-            info.put("BALANCE", 0);
-            account.put(acc, info);
-            String url = "jdbc:sqlite:" + fileName;
-            String sql = "INSERT INTO card (number, pin) VALUES(?,?)";
-
-            try (Connection conn = DriverManager.getConnection(url);
-                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-                // Setting parameters
-
-                pstmt.setString(1, acc);
-                pstmt.setInt(2, Integer.parseInt(pin.toString()));
-                pstmt.executeUpdate();
-
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-
-            }
-            return acc;
-        }
-
-        private int getChecksum() {
-            int sum = 0;
-            for (int i = 0; i < acc.length(); i++) {
-                int digit = Character.getNumericValue(acc.charAt(i));
-                if (i % 2 == 0) {
-                    digit *= 2;
-                }
-                if (digit > 9) {
-                    digit -= 9;
-                }
-                sum += digit;
-            }
-            int checksum = 10 - sum % 10;
-            if (checksum % 10 == 0) {
-                checksum = 0;
-            }
-            return checksum;
-        }
-
-        void exit() {
-            System.out.println("\nBye!");
-            scanner.close();
-        }
+    void exit() {
+        System.out.println("\nBye!");
+        System.exit(0);
     }
 }
